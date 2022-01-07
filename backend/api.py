d49de94881962bafa7668dd58d0c6907eade07bd
MatthiasLHK <matthiasli1998@hotmail.com>
2022-01-07 17:52:45 +0800

    wip

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,6 @@
 import time
 from flask import Flask, request
-from intersg import main as m1
+from internsg import main as m1
 from linkedin import search as m2
 
 app = Flask(__name__)
@@ -13,9 +13,9 @@ def test():
 def get_current_time():
     return {'time': time.time()}
 
-@app.route('/internsg/keyword')
-def get_data_internsg():
-    return m1("keyword")
+@app.route('/internsg/<keyword>')
+def get_data_internsg(keyword):
+    return m1(keyword)
 
 @app.route('/linkedin/<id>')
 def get_data_linkedin(id):
---
+++ backend/internsg.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup as BS
+import json
+
+def main(keyword):
+    url = f"https://www.internsg.com/jobs/1/?f_p&f_i&filter_s{keyword}#isg-top"
+
+    request = requests.get(url)
+    page = BS(request.text, "html.parser")
+    store1 = page.findAll("div", {"class":"ast-row list-even"})
+    result = []
+    for i in store1:
+        t1 = i.findAll("div", {"class":"ast-col-lg-3"})
+        t2 = i.findAll("div", {"class":"ast-col-lg-2"})
+        t3 = i.findAll("div", {"class":"ast-col-lg-1"})
+        name = t1[0].text
+        domain = t1[0].span.text
+        title = t1[1].text
+        index = name.find(domain)
+        name = name[:index]
+        date = t3[0].text
+        temp = {}
+        temp[name] = {}
+        temp[name]["title"] = title.strip()
+        temp[name]["date"] = date.strip()
+        result.append(temp)
+    store2 = page.findAll("div", {"class":"ast-row list-odd"})
+    for i in store2:
+        t1 = i.findAll("div", {"class":"ast-col-lg-3"})
+        t2 = i.findAll("div", {"class":"ast-col-lg-2"})
+        t3 = i.findAll("div", {"class":"ast-col-lg-1"})
+        name = t1[0].text
+        domain = t1[0].span.text
+        title = t1[1].text
+        index = name.find(domain)
+        name = name[:index]
+        date = t3[0].text
+        temp = {}
+        temp[name] = {}
+        temp[name]["title"] = title.strip()
+        temp[name]["date"] = date.strip()
+        result.append(temp)
+    return result
+print(main(""))
