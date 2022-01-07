d2f26f113e9c2149011f4f18f345d0da85b07a64
MatthiasLHK <matthiasli1998@hotmail.com>
2022-01-07 12:08:58 +0800

    add root path for backend

--- backend/api.py
+++ backend/api.py
@@ -3,10 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def test():
+    return {'name': "server is up and running"}
+
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
-
-
 
 print("hi")
