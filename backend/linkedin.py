1d6637099949550a79f79be71e1ed6af556f0146
davidcaiqifan <caid100@gmail.com>
2022-01-07 17:56:15 +0800

    Add aggregate api

--- backend/linkedin.py
+++ backend/linkedin.py
@@ -14,16 +14,17 @@ def search(keyword):
 
         # for sg only
         source = requests.get('https://www.linkedin.com/jobs/search/?geoId=102454443&keywords=' + kw + page).text
-        print('https://www.linkedin.com/jobs/search/?geoId=102454443&keywords=' + kw + page)
+        # print('https://www.linkedin.com/jobs/search/?geoId=102454443&keywords=' + kw + page)
         soup = BeautifulSoup(source, 'lxml')
         jobs = soup.findAll('div', class_='base-card base-card--link base-search-card base-search-card--link job-search-card')
 
         for job in jobs:
             try:
+                date = job
                 time = job.time.text.strip()
                 # print(time)
                 title = job.a.text.strip()
-                print(title)
+                # print(title)
                 company = job.find('a', class_='hidden-nested-link').text.strip()
                 link = job.a['href'].strip()
                 add = {"company" : company, "title" : title, "link" : link, "time" : time}
@@ -36,5 +37,5 @@ def search(keyword):
     # print(result)
     return json.dumps(result)
 
-search('software engineer intern')
+# search('software engineer intern')
 
