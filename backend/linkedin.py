9ad0b55d82456b93bab1b40c9a56c5746f0df1d9
davidcaiqifan <caid100@gmail.com>
2022-01-07 22:09:02 +0800

    Update linkedin

--- backend/linkedin.py
+++ backend/linkedin.py
@@ -13,7 +13,8 @@ def search(keyword):
         page = '&location=Singapore&start=' + str(i * 25)
 
         # for sg only
-        source = requests.get('https://www.linkedin.com/jobs/search/?geoId=102454443&keywords=' + kw + page).text
+        # source = requests.get('https://www.linkedin.com/jobs/search/?geoId=102454443&keywords=' + kw + page).text
+        source = requests.get('https://www.linkedin.com/jobs/search/?keywords=' + kw + '&location=Singapore' + page).text
         # print('https://www.linkedin.com/jobs/search/?geoId=102454443&keywords=' + kw + page)
         soup = BeautifulSoup(source, 'lxml')
         jobs = soup.findAll('div', class_='base-card base-card--link base-search-card base-search-card--link job-search-card')
